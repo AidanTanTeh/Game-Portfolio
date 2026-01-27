46db4ef4bc9e06e57f621fc3eec8f8fe857fcaa4
Aidan Tan Teh E Shin <z5634952@ad.unsw.edu.au>
2026-01-26 19:01:03 -0800

    setup main canvas for game

---
+++ src/Resource.js
@@ -0,0 +1,29 @@
+class Resources {
+    constructor() {
+        // Everything we plan to download
+        this.toLoad = {
+            hero: "./sprites/hero-sheet.png",
+            ground: "./sprites/ground.png",
+            sky: "./sprites/sky.png"
+        }
+
+        // Bucket to keep all images
+        this.images = {};
+
+        // Load each image
+        Object.keys(this.toLoad).forEach(key => {
+            const img = new Image();
+            img.src = this.toLoad[key];
+            this.images[key] = {
+                image: img,
+                isLoaded: false
+            }
+            img.onload = () => {
+                this.images[key].isLoaded = true;
+            }
+        })
+    };
+}
+
+// Create one instance for the whole app to use
+export const resources = new Resources();
